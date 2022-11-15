54da5ecf2a609417c619b80a9dffe8c18ed16ff3
nast0yka <anse.soroka@gmail.com>
2022-11-15 05:51:25 +0300

    домашнее задание 5
    1.фунция - рандомайзер чисел
    2.выбор предсказания
    3. обработчик клика с функцией вывода нового текста
    4. функция добавления в шаблон предсказания (не работате)

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
-
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
-
 /* Подставляй текст нового предсказания в .current-forecast h1 */
-
 /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
-
 /* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
-
 /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
-
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
-
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+function generate(min, max) {
+    let a = Math.random() * (max - min) + min;
+    return Math.round(a);
+}
+
+function generatePrediction() {
+    switch (generate(1, 3)) {
+        case 1:
+            return "Ты решишь эту задачу";
+        case 2:
+            return "Ты наконец-то сделаешь тренажер без правок Полины";
+        case 3:
+            return "Ты будешь крутым Frontend-разработчиком";
+    }
+}
+
+const currentForecast = document.querySelector('.current-forecast');
+const forecastContainer = document.querySelector('.forecast-item');
+
+let button = document.querySelector('.forecast-btn');
+
+button.addEventListener('click', function() {
+    let a = currentForecast.querySelector('h1').textContent = generatePrediction();
+    let b = currentForecast.querySelector('p').textContent = `Вероятность ${generate(0, 100)} %`;
+
+    function creataNewForecast(a, b) {
+        const template = document.querySelector("#forecast-item");
+        const newForeCast = template.content.cloneNode(true);
+        newForeCast.querySelector('h3').textContent = a;
+        newForeCast.querySelector('p').textContent = b;
+        forecastContainer.append(newForeCast);
+    }
+    creataNewForecast(a, b);
+});
